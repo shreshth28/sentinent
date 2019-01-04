b2def9eab76623c7493d44beadad42a8f498dd7e
Shreshth <shreshth28srivastava@gmail.com>
2019-01-05 02:10:18 +0530

    Updates

---
+++ precache-manifest.07cc1d805ec6fa90838f4dde8a916296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sentinent/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3d3f2c5a150b16d1e54",
+    "url": "/sentinent/static/js/runtime~main.d3d3f2c5.js"
+  },
+  {
+    "revision": "08b717689364bb2a92e8",
+    "url": "/sentinent/static/js/main.08b71768.chunk.js"
+  },
+  {
+    "revision": "59e4efbacf38ac1e4c41",
+    "url": "/sentinent/static/js/1.59e4efba.chunk.js"
+  },
+  {
+    "revision": "08b717689364bb2a92e8",
+    "url": "/sentinent/static/css/main.ee219120.chunk.css"
+  },
+  {
+    "revision": "59e4efbacf38ac1e4c41",
+    "url": "/sentinent/static/css/1.562d307c.chunk.css"
+  },
+  {
+    "revision": "2832b997f540d79e90084b405caaf7c8",
+    "url": "/sentinent/index.html"
+  }
+];
